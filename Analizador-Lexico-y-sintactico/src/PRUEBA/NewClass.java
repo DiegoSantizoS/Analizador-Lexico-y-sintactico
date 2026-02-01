5bd017594a923e5d779465fb2bf5a42709dad4df
Carlos Andres <Carlos Andres@DESKTOP-NAG3ARL>
2026-01-31 20:33:50 -0600

    so many worlds

--- Analizador-Lexico-y-sintactico/src/PRUEBA/NewClass.java
+++ Analizador-Lexico-y-sintactico/src/PRUEBA/NewClass.java
@@ -13,5 +13,14 @@ package PRUEBA;
 public class NewClass {
 public static void main (String[] args){        
     System.out.println("Hello World");
+    System.out.println("Hello World");
+    System.out.println("Hello World");
+    System.out.println("Hello World");
+    System.out.println("Hello World");System.out.println("Hello World");System.out.println("Hello World");
+    System.out.println("Hello World");
+    System.out.println("Hello World");
+    
+    
+    
     }
 }

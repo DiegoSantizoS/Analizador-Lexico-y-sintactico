14942e4a119c81429f551d5e49435f9dc99e8b33
DFSS <DFSS@DESKTOP-A3JRM2U>
2026-02-01 00:57:04 -0600

    NewClass archivo de prueba

--- Analizador-Lexico-y-sintactico/src/PRUEBA/NewClass.java
+++ Analizador-Lexico-y-sintactico/src/PRUEBA/NewClass.java
@@ -1,18 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package PRUEBA;
 
-/**
- *lol xd 
- * How many dudes
- * @author DFSS
- * LOLOLOLOLOLOL
- * NNNNNNNNNNNNOOOOOOOOOOOOOOOOOOOOOOO
- */ 
+import generated.LenguajeLexer;
+import generated.LenguajeParser;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
 public class NewClass {
-public static void main (String[] args){        
-    System.out.println("Hello World");
+    public static void main(String[] args) throws Exception {
+
+        // Example input valid for your grammar:
+        String input = """
+            int x = 5;
+            print x;
+            while (x > 0) {
+                x = x - 1;
+                print x;
+            }
+            """;
+
+        CharStream cs = CharStreams.fromString(input);
+
+        // 1) Lexer
+        LenguajeLexer lexer = new LenguajeLexer(cs);
+
+        // (Optional) Token test
+        System.out.println("TOKENS:");
+        for (Token t = lexer.nextToken(); t.getType() != Token.EOF; t = lexer.nextToken()) {
+            String type = lexer.getVocabulary().getSymbolicName(t.getType());
+            System.out.println(t.getText() + " -> " + type);
+        }
+
+        // Reset the input for parsing (because we consumed tokens above)
+        cs = CharStreams.fromString(input);
+        lexer = new LenguajeLexer(cs);
+
+        // 2) Parser
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        LenguajeParser parser = new LenguajeParser(tokens);
+
+        // Your start rule:
+        ParseTree tree = parser.program();
+
+        System.out.println("\nPARSE TREE:");
+        System.out.println(tree.toStringTree(parser));
     }
 }
